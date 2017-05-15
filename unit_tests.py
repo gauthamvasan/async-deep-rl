286feb6637cf26ddcd7e9458a0c5efd221cdef84
Gautham Vasan <gauthamv.529@gmail.com>
2017-05-15 14:58:57 -0600

    Unit tests for model

--- unit_tests.py
+++ unit_tests.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Disables debuugging logs of tensorflow
+
 import unittest
 from atari_wrapper import  Atari_Environment
 import gym
+import tensorflow as tf
+from build_model import sequential_network
+import numpy as np
+from keras import backend as K
 
 class Atari_Tests(unittest.TestCase):
     def test(self):
@@ -15,4 +24,17 @@ class Atari_Tests(unittest.TestCase):
 
 class Model_Tests(unittest.TestCase):
     def test(self):
-        raise NotImplementedError
+        g = tf.Graph()
+        session = tf.Session()
+        K.set_session(session)
+        state = tf.placeholder("float",
+                               [None, 4, 84, 84])
+        q_network = sequential_network(num_actions = 6, history_length = 4, scaled_width = 84, scaled_height = 84)
+        network_params = q_network.trainable_weights
+        q_values = q_network(state)
+
+        st = np.zeros((4,84,84))
+        session.run(tf.global_variables_initializer())
+
+        q_vals = q_values.eval(session=session, feed_dict={state: [st]})
+        self.assertEqual(q_vals.shape,(1,6))
